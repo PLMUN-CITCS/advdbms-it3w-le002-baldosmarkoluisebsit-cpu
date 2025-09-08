714d49f068ebf0f3fd1f7f074cb5700cffaf9d58
MarkoBaldos <baldosmarkoluise_bsit@plmun.edu.ph>
2025-09-08 11:23:30 +0800

    Submitting ADVDBMS Week 04 - Session 01 - Exercise 02

--- university_db/create_and_use_db.sql
+++ university_db/create_and_use_db.sql
@@ -1,19 +1,6 @@
-<<<<<<< HEAD
-CREATE DATABASE UniversityDB;
-USE UniversityDB;
-CREATE TABLE Students (
-    StudentID INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    FirstName VARCHAR(50) NOT NULL,
-    LastName VARCHAR(50) NOT NULL,
-    EnrollmentDate DATE,
-    Email VARCHAR(100)
-=======
-USE UniversityDB;
-
-CREATE TABLE IF NOT EXISTS `Students` (
-   `StudentID` INT PRIMARY KEY AUTO_INCREMENT,
-   `FirstName` VARCHAR(50) NOT NULL,
-   `LastName` VARCHAR(50) NOT NULL,
-   `EnrollmentDate` DATE
->>>>>>> 8da48c76256d2fef586c1a85d1dbc869774eab9c
-);
+StudentID int(11) NO PRI NULL auto_increment;
+FirstName varchar(50) NO NULL ;
+LastName varchar(50) NO NULL ;
+EnrollmentDate date YES NULL ;
+Email varchar(100) YES NULL ;
+Actual:
