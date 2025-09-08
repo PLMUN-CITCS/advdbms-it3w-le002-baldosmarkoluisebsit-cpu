9b7c13421d1e499f43eccf48c66d784979d5b6f8
MarkoBaldos <baldosmarkoluise_bsit@plmun.edu.ph>
2025-09-08 12:14:44 +0800

    Submitting ADVDBMS Week 04 - Session 01 - Exercise 02

--- university_db/alter_students_table.sql
+++ university_db/alter_students_table.sql
@@ -1,5 +1,4 @@
-Use UniversityDB;
+USE UniversityDB;
 
--- Step 2: Alter the table to add an Email column
 ALTER TABLE `Students`
-ADD Email VARCHAR(100);
+ADD `Email` VARCHAR(100);
--- university_db/create_students_table.sql
+++ university_db/create_students_table.sql
@@ -1,7 +1,8 @@
-CREATE TABLE Students (
-    StudentID INT(11) PRIMARY KEY AUTO_INCREMENT,
-    FirstName VARCHAR(50) NOT NULL,
-    LastName VARCHAR(50) NOT NULL,
-    EnrollmentDate DATE,
-    Email VARCHAR(100)
+USE UniversityDB;
+
+CREATE TABLE `Students` (
+    `StudentID` INT PRIMARY KEY AUTO_INCREMENT,
+    `FirstName` VARCHAR(50) NOT NULL,
+    `LastName` VARCHAR(50) NOT NULL,
+    `EnrollmentDate` DATE
 );
