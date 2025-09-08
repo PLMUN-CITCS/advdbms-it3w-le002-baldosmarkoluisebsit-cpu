21aaf9c5f4983fca53f12b4b612e6e06c6666dda
MarkoBaldos <baldosmarkoluise_bsit@plmun.edu.ph>
2025-09-08 09:32:34 +0800

    Submitting ADVDBMS Week 04 - Session 01 - Exercise 02

--- university_db/create_students_table.sql
+++ university_db/create_students_table.sql
@@ -1,6 +1,8 @@
-StudentID int(11) NO PRI NULL auto_increment;
-FirstName varchar(50) NO NULL ;
-LastName varchar(50) NO NULL ;
-EnrollmentDate date YES NULL ;
-Email varchar(100) YES NULL ;
-Actual:
+USE UniversityDB;
+
+CREATE TABLE IF NOT EXISTS `Students` (
+   `StudentID` INT PRIMARY KEY AUTO_INCREMENT,
+   `FirstName` VARCHAR(50) NOT NULL,
+   `LastName` VARCHAR(50) NOT NULL,
+   `EnrollmentDate` DATE
+);
